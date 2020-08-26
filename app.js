7707528f98d41c1aea6b6f60b61313960661a2ef
saltemose <sean@seanaltemose.com>
2020-08-26 11:25:42 -0400

    finalize start

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const passport = require('passport');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
+
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>MERN STACK TEMPLATE</h1>
+        <footer>
+       
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
